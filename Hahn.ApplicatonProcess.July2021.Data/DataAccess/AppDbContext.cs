61fee531b5bc6c967c05136774780a7b5bc6ad79
unknown <gustavolonda@gmail.com>
2021-07-24 01:02:45 -0500

    Create DBContext

---
+++ Hahn.ApplicatonProcess.July2021.Data/DataAccess/AppDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hahn.ApplicatonProcess.July2021.Data.Models;
+
+namespace Hahn.ApplicatonProcess.July2021.Data.DataAccess
+{   /********************************************************
+    *                         Context                       *
+    *********************************************************/
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options){}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Asset> Assets { get; set; }
+    }
+}
