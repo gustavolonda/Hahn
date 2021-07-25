2a3672af563506c7365e2ca5b231a2e05f036312
unknown <gustavolonda@gmail.com>
2021-07-24 19:54:58 -0500

    Create WebApi

--- Hahn.ApplicatonProcess.July2021.Web/Startup.cs
+++ Hahn.ApplicatonProcess.July2021.Web/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-
+using  Hahn.ApplicatonProcess.July2021.Data.DataAccess;
 namespace Hahn.ApplicatonProcess.July2021.Web
 {
     public class Startup
@@ -26,12 +26,13 @@ namespace Hahn.ApplicatonProcess.July2021.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hahn.ApplicatonProcess.July2021.Web", Version = "v1" });
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
